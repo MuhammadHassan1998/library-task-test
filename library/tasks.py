d7411ef7c7d60d73f729f0714a94fc098b5ffb3f
apple <apple@192.168.1.25>
2025-09-18 02:18:03 +0500

    feat: test tasks

---
+++ library/tasks.py
@@ -0,0 +1,47 @@
+from celery import shared_task
+from .models import Loan
+from django.core.mail import send_mail
+from django.conf import settings
+from datetime import date
+
+@shared_task
+def send_loan_notification(loan_id):
+    try:
+        loan = Loan.objects.get(id=loan_id)
+        member_email = loan.member.user.email
+        book_title = loan.book.title
+        send_mail(
+            subject='Book Loaned Successfully',
+            message=f'Hello {loan.member.user.username},\n\nYou have successfully loaned "{book_title}".\nPlease return it by the due date.',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[member_email],
+            fail_silently=False,
+        )
+    except Loan.DoesNotExist:
+        pass
+
+def check_overdue_loans(loan_id):
+    today = date.today()
+    try:
+        overdue_loans= Loan.object.filter(
+            is_returned=False,
+            due_date_It=today
+        ).select_related('member', 'book', 'due_date')
+        # iterate through loaned books and send email to each
+        for loan in overdue_loans:
+            try:
+                
+                member_email = loan.member.user.email
+                due_date = loan.due_date
+                days_overdue = (today - due_date).days
+                send_mail(
+                    subject='Reminder for Book Loaned',
+                    message=f'Hello {loan.member.user.username}, gental reminder that your loan is over dues by {days_overdue}',
+                    from_email=settings.DEFAULT_FROM_EMAIL,
+                    recipient_list=[member_email],
+                    fail_silently=False,
+                )
+            except:
+                pass
+    except Loan.DoesNotExist:
+        pass
